6c37c165c8f474dddc5d783b6135c20794d48abe
longtime1116 <longtime1116@gmail.com>
2017-07-18 22:41:14 +0900

    rails_5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -26,4 +26,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
   end
 
+  test "should get signup" do
+    get signup_path
+    assert_response :success
+    assert_select "title", full_title("Sign up")
+  end
+
 end
