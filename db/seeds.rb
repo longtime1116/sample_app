8424a411725b000d0f6212121eb7843334918f97
longtime1116 <longtime1116@gmail.com>
2017-07-25 23:19:07 +0900

    rails_11.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,25 @@ User.create!(name:  "longtime",
              email: "longtime@gmail.com",
              password:              "hogehoge",
              password_confirmation: "hogehoge",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 User.create!(name:  "hoge",
              email: "hoge@gmail.com",
              password:              "hogehoge",
              password_confirmation: "hogehoge",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: false)
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
 
 98.times do |n|
   name  = Faker::Name.name
@@ -23,5 +29,9 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               admin: true,
+               activated: true,
+               activated_at: Time.zone.now)
+
 end
