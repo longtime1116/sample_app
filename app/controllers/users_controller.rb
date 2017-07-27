1aa11c2a3f5317391edd8515d13d16cec1605eea
longtime1116 <longtime1116@gmail.com>
2017-07-27 16:10:06 +0900

    rails_13.3.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,14 +55,6 @@ class UsersController < ApplicationController
 
     # before アクション
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインして下さい"
-        redirect_to login_url
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
